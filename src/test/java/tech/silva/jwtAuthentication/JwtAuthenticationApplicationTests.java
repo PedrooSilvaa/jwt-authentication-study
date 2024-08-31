9d0cd4e0249c8a0e5e3f82597ec3d61f63b5aaab
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-08-31 20:10:33 -0300

    feat: create project add dependencies

---
+++ src/test/java/tech/silva/jwtAuthentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.jwtAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
