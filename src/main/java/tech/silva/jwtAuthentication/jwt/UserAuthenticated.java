0a17950378cde85d512e896c432703836c1635d2
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-08-31 20:36:21 -0300

    feat: create UserAuthenticated and implement UserDetails methods

---
+++ src/main/java/tech/silva/jwtAuthentication/jwt/UserAuthenticated.java
@@ -0,0 +1,52 @@
+package tech.silva.jwtAuthentication.jwt;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import tech.silva.jwtAuthentication.entity.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserAuthenticated implements UserDetails {
+
+    private final User user;
+
+    public UserAuthenticated(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "read");
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/tech/silva/jwtAuthentication/repository/UserRepository.java
+++ src/main/java/tech/silva/jwtAuthentication/repository/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, String> {
 
-    Optional<User> findUsername(String username);
+    Optional<User> findByUsername(String username);
 }
