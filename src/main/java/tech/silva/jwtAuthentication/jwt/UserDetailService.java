53876279d8ad9e390cca1e5dfd565dca1e7fe9f7
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-08-31 20:39:39 -0300

    feat: create UserDetailsService

---
+++ src/main/java/tech/silva/jwtAuthentication/jwt/UserDetailService.java
@@ -0,0 +1,22 @@
+package tech.silva.jwtAuthentication.jwt;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import tech.silva.jwtAuthentication.repository.UserRepository;
+
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserAuthenticated::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
