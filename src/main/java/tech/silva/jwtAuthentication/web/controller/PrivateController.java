b468965d3c46d2e8a1556737931e4107f2690e90
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-08-31 20:15:13 -0300

    feat: create PrivateController Create private route controller

---
+++ src/main/java/tech/silva/jwtAuthentication/web/controller/PrivateController.java
@@ -0,0 +1,16 @@
+package tech.silva.jwtAuthentication.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("private")
+public class PrivateController {
+
+    @GetMapping
+    public String getMessage(){
+        return "Hello from private API Controller";
+    }
+
+}
